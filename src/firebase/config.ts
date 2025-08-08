6151b52750790cdd9f0a28318e11406712f2880a
jayesh-bhai <unknownjayesh05@gmail.com>
2025-08-08 16:14:21 +0530

    removed firebase credentials from repo

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-// Firebase config only
-export const firebaseConfig = {
-  apiKey: "AIzaSyD6Q0fT01DJD25hNAa-kUFKrLIp2IWNGr0",
-  authDomain: "on-demand-tutor-3d634.firebaseapp.com",
-  projectId: "on-demand-tutor-3d634",
-  storageBucket: "on-demand-tutor-3d634.appspot.com",
-  messagingSenderId: "345039363622",
-  appId: "1:345039363622:web:55fab781fe4c36355390e7"
-};
